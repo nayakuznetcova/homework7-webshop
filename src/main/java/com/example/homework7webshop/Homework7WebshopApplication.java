542055c442a8bad071a77793e6a5d5a762d20aed
Admin <samedi450511@gmail.com>
2023-07-31 23:45:46 +0600

    start

---
+++ src/main/java/com/example/homework7webshop/Homework7WebshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework7webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework7WebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework7WebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework7webshop/Homework7WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework7webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework7WebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
