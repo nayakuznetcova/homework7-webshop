e6df02d5b56780d0d4c93bd9c44710f35671b54e
Admin <samedi450511@gmail.com>
2023-08-02 18:52:14 +0600

    done

---
+++ src/main/java/com/example/homework7webshop/service/BasketServiceInterface.java
@@ -0,0 +1,9 @@
+package com.example.homework7webshop.service;
+
+import java.util.Collections;
+import java.util.List;
+
+public interface BasketServiceInterface {
+    void add(List<Integer> list);
+    List<Integer> get();
+}
